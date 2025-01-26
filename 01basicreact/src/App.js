01d2d49a081e652c7dbbde6e50ae90dbe440efdd
Leeladhar <139422865+leeladhar052@users.noreply.github.com>
2025-01-26 18:45:02 +0530

    Your commit message here

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import Chai from './Chai'
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>chai or code</h1>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,18 @@
+import Chai from "./chai.jsx"
+
+
+function App() {
+
+  return (
+    //fragmentation
+    <>  
+    <Chai/>
+    <h1>chai aur react</h1>
+    <p>test para</p>
+    </>
+    
+
+  )
+}
+
+export default App
